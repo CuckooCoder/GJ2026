480a988d1e5d50b7a1e3de41881c79a4d41d0e1d
Tang Yuanyuan <dee243741190@gmail.com>
2026-07-05 12:11:04 +0800

    修

--- Assets/Scripts/GamePlay/Opening/OpeningManager.cs
+++ Assets/Scripts/GamePlay/Opening/OpeningManager.cs
@@ -6,67 +6,47 @@ using UnityEngine.Video;
 
 public class OpeningManager : MonoBehaviour
 {
-	public List<VideoClip> videoClips;
-	public List<string> lines;
-	public TMP_Text textUI;
+    public List<VideoClip> videoClips;
+    public List<string> lines;
+    public TMP_Text textUI;
 
+    public VideoPlayer videoPlayer;
 
-	public VideoPlayer videoPlayer;
+    private int currentIndex = 0;
 
-	// 当前播放索引
-	private int currentIndex = 0;
-	// 标记：视频是否已经播放完成，等待点击
-	private bool waitForClick = false;
+    void Start()
+    {
+        TransitionEffect.Instance.FadeIn();
 
-	void Start()
-	{
-		TransitionEffect.Instance.FadeIn();
-		// 绑定播放结束事件
-		videoPlayer.loopPointReached += OnVideoFinish;
+        videoPlayer.loopPointReached += OnVideoFinish;
 
-		if (videoClips.Count > 0)
-		{
-			PlayVideo(currentIndex);
-		}
-	}
+        if (videoClips.Count > 0)
+        {
+            PlayVideo(currentIndex);
+        }
+    }
 
-	// 播放指定索引视频
-	void PlayVideo(int index)
-	{
-		waitForClick = false;
-		videoPlayer.clip = videoClips[index];
-		videoPlayer.Play();
-		textUI.text = lines[index];
-	}
+    void PlayVideo(int index)
+    {
+        videoPlayer.clip = videoClips[index];
+        videoPlayer.Play();
 
-	// 视频播放完毕回调
-	void OnVideoFinish(VideoPlayer vp)
-	{
-		waitForClick = true;
-	}
+        if (index < lines.Count)
+            textUI.text = lines[index];
+    }
 
-	void Update()
-	{
-		// 只有视频播完等待点击时才响应鼠标左键/屏幕点击
-		if (waitForClick && Input.GetMouseButtonDown(0))
-		{
-			NextVideo();
-		}
-	}
+    void OnVideoFinish(VideoPlayer vp)
+    {
+        currentIndex++;
 
-	void NextVideo()
-	{
-		currentIndex++;
-
-		// 判断是否还有下一个视频
-		if (currentIndex < videoClips.Count)
-		{
-			PlayVideo(currentIndex);
-		}
-		else
-		{
-			Debug.Log("所有视频播放完毕");
-			GameManager.Instance.LoadScene(GameManager.Instance.curLevelIndex + 1);
-		}
-	}
+        if (currentIndex < videoClips.Count)
+        {
+            PlayVideo(currentIndex);
+        }
+        else
+        {
+            Debug.Log("所有视频播放完毕");
+            GameManager.Instance.LoadScene(GameManager.Instance.curLevelIndex + 1);
+        }
+    }
 }
